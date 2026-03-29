da7acfa42db1e2071e98c9a3fcaa1af0971e5bde
Mayara Fernandes <mayarafernandes.al@hotmail.com>
2026-03-29 14:54:52 -0300

    Adicona imagens e trailers

---
+++ catalogo/js/data.js
@@ -0,0 +1,41 @@
+export const categories = [
+    {
+        title: "Mais assistidos",
+        items: [
+            {
+                img: "https://m.media-amazon.com/images/M/MV5BZGM2ZTRlNWEtZjhjNC00NGVjLWE5OTktYmRkNTdlNmVmMDEzXkEyXkFqcGdeQVRoaXJkUGFydHlJbmdlc3Rpb25Xb3JrZmxvdw@@._V1_.jpg", top10: true, badge: "Clássico", badgeColor: "red", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=MySk5QL3eWM"
+            },
+            {
+                img: "https://m.media-amazon.com/images/M/MV5BMTYzMDM4NzkxOV5BMl5BanBnXkFtZTgwNzM1Mzg2NzM@._V1_FMjpg_UX1000_.jpghttps://aventurasnahistoria.com.br/wp-content/uploads/entretenimento/gladiador_2_VvnGVes.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=76CslX-q5C4"
+            },
+            {
+                img: "https://rollingstone.com.br/wp-content/uploads/emma_watson_rupert_grint_e_daniel_radcliffe_em_harry_potter_e_a_pedra_filosofal_foto_divulgacao.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=9fIObnIGMlI"
+            },
+            {
+                img: "https://m.media-amazon.com/images/I/81CIXJxQ3TL._AC_UF1000,1000_QL80_.jpg", progress: 0,
+                youtube: "https://www.youtube.com/watch?v=vZ734NWnAHA"
+            },
+        ]
+    },
+    {
+        title: "Séries",
+        items: [
+            { img: "https://conectageek.com.br/wp-content/uploads/2025/04/Se-a-Vida-Te-Der-Tangerinas-Foto-Reproducao-Netflix.jpg", top10: true, badge: "Nova temporada", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://ingresso-a.akamaihd.net/b2b/production/uploads/article/image/1313/getting-lost-documentario-serie-lost-b05ac06f1eff26.jpg", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://disney.images.edge.bamgrid.com/ripcut-delivery/v2/variant/disney/ca99f138-fc43-49e0-8911-d57cba93c2fb/compose?aspectRatio=1.78&format=webp&width=1200", badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://lh3.googleusercontent.com/IFW6Uqn-rWpCAVbt7oKPcE_nf5smCrERwpWTLaNy__YvEzYUXDeoiIqEqCtJ7SjNV2lwgx42DMQ=w1440-ns-nd-rj", badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    },
+    {
+        title: "Para maratonar",
+        items: [
+            { img: "https://i.redd.it/the-michael-eating-popcorn-meme-old-screenshot-vs-new-one-v0-qekp489izn0a1.jpg?width=720&format=pjpg&auto=webp&s=dbf2b1e87bca695d435d63adb1bb58304fd09f5f", top10: true, youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://i.redd.it/the-michael-eating-popcorn-meme-old-screenshot-vs-new-one-v0-qekp489izn0a1.jpg?width=720&format=pjpg&auto=webp&s=dbf2b1e87bca695d435d63adb1bb58304fd09f5f", top10: true, badge: "Novidade", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://i.redd.it/the-michael-eating-popcorn-meme-old-screenshot-vs-new-one-v0-qekp489izn0a1.jpg?width=720&format=pjpg&auto=webp&s=dbf2b1e87bca695d435d63adb1bb58304fd09f5f", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+            { img: "https://i.redd.it/the-michael-eating-popcorn-meme-old-screenshot-vs-new-one-v0-qekp489izn0a1.jpg?width=720&format=pjpg&auto=webp&s=dbf2b1e87bca695d435d63adb1bb58304fd09f5f", top10: true, badge: "Novo episódio", badgeColor: "red", youtube: "https://www.youtube.com/watch?v=wLo9bfgla4k" },
+        ]
+    }
+];
